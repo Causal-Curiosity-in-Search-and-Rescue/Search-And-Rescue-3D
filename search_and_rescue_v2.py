d93e889b965f4621d581f8540dd30086cde401fb
juliangdz <juliangeralddcruz@outlook.com>
2024-03-14 12:48:25 +0000

    added non graphical interface

--- search_and_rescue_v2.py
+++ search_and_rescue_v2.py
@@ -41,7 +41,7 @@ ENV_MANAGER = EnvironmentObjectsManager()
 # Define How Long the Robot should be Operational in the Environment
 AGENT_ACTION_LEN = 30
 # p.connect(p.GUI)
-p.connect()
+p.connect(p.DIRECT)
 
 height = 20
 width = 20
